f82cd964ea037416018f0948c9025949cb8e0805
Benjamin Rackham <brackham@mit.edu>
2020-07-03 20:32:20 -0400

    Spiffy it up a bit

--- transitforecast/__init__.py
+++ transitforecast/__init__.py
@@ -1,3 +1,15 @@
-from .forecast import *
-from .modeling import *
-from .plots import *
+"""
+An open-source package for probabilistic forecasting of candidate
+exoplanetary transit signals.
+
+* Code: https://github.com/disruptiveplanets/transitforecast
+"""
+from .forecast import *  # noqa
+from .modeling import *  # noqa
+from .plots import *  # noqa
+
+__uri__ = "https://docs.exoplanet.codes"
+__author__ = "Benjamin Rackham"
+__email__ = "brackham@mit.edu"
+__license__ = "MIT"
+__description__ = "Forecast candidate exoplanetary transits"
